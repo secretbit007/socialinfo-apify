cee25fc688e43531837c1843b1a1c8763909387d
secretbit007 <secretbit007@gmail.com>
2025-03-19 02:27:54 +0900

    Fix log issue

--- src/main.py
+++ src/main.py
@@ -11,38 +11,38 @@ from apify import Actor
 
 async def main() -> None:
     async with Actor:
-        await Actor.log.info('Dropping socialinfo dataset...')
+        Actor.log.info('Dropping socialinfo dataset...')
         dataset = await Actor.open_dataset(name='socialinfo')
         await dataset.drop()
 
-        await Actor.log.info('Creating socialinfo dataset...')
+        Actor.log.info('Creating socialinfo dataset...')
         dataset = await Actor.open_dataset(name='socialinfo')
 
-        await Actor.log.info('Scraping results...')
+        Actor.log.info('Scraping results...')
 
-        await Actor.log.info('sozialinfo')
+        Actor.log.info('sozialinfo')
         await scrape_sozialinfo_data(dataset)
 
-        await Actor.log.info('gemeindestellen')
+        Actor.log.info('gemeindestellen')
         await scrape_gemeindestellen_data(dataset)
 
-        await Actor.log.info('heiminfo')
+        Actor.log.info('heiminfo')
         await scrape_heiminfo_data(dataset)
 
-        await Actor.log.info('jobs')
+        Actor.log.info('jobs')
         await scrape_jobs_data(dataset)
 
-        await Actor.log.info('sozjobs')
+        Actor.log.info('sozjobs')
         await scrape_sozjobs_data(dataset)
 
-        await Actor.log.info('stiftungschweiz')
+        Actor.log.info('stiftungschweiz')
         await scrape_stiftungschweiz_data(dataset)
 
-        await Actor.log.info('workswiss')
+        Actor.log.info('workswiss')
         await scrape_workswiss_data(dataset)
 
-        await Actor.log.info('publicjobs')
+        Actor.log.info('publicjobs')
         await scrape_publicjobs_data(dataset)
 
-        await Actor.log.info('google')
+        Actor.log.info('google')
         await scrape_google_data(dataset)
