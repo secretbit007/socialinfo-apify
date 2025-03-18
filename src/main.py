de23374393872a865e9c42f030d52f32eda3b5a2
secretbit007 <secretbit007@gmail.com>
2025-03-19 01:59:34 +0900

    dataset recreate

--- src/main.py
+++ src/main.py
@@ -7,8 +7,15 @@ from .publicjobs import scrape_publicjobs_data
 from .sozjobs import scrape_sozjobs_data
 from .stiftungschweiz import scrape_stiftungschweiz_data
 from .workswiss import scrape_workswiss_data
+from apify import Actor
 
 async def main() -> None:
+    async with Actor:
+        dataset = await Actor.open_dataset(name='socialinfo')
+        await dataset.drop()
+
+        await Actor.open_dataset(name='socialinfo')
+
     await scrape_sozialinfo_data()
     await scrape_gemeindestellen_data()
     await scrape_heiminfo_data()
